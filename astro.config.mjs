b2f08c4d9378ed25315073ff71217ba4f4ee58ff
Fawwaz <gesitee@users.noreply.github.com>
2026-08-26 20:48:28 +0700

    Add GSC verification file and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sayuronlinebantul.com',
+  site: 'https://sayuronline.vercel.app',
   integrations: [
     tailwind(),
     sitemap()
